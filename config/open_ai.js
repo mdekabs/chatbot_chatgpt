31426dcb471cce99174e67b6840031d96cf2354f
mdekabs <1mawoda@gmail.com>
2023-10-17 16:46:20 +0100

    correction

--- config/open_ai.js
+++ config/open_ai.js
@@ -1,10 +1,10 @@
-import OpenAI from 'openai';
-import dotenv from "dotenv";
+import { OpenAI } from 'openai';
+import dotenv from 'dotenv';
+
 dotenv.config();
-//const apiKey = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI({
-  apiKey: process.env["OPENAI_API_KEY"]
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export default openai;
